90319eb633faf129b62fbd6600ee60a8a5a946d4
Log <91562887+LogETH@users.noreply.github.com>
2022-07-07 18:32:06 -0700

    Update SafeERC20.sol

--- SafeERC20.sol
+++ SafeERC20.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0 <0.7.0;
 
 import {SafeMath} from "https://github.com/LogETH/imports/blob/main/SafeMath.sol";
 import {Address} from "https://github.com/LogETH/imports/blob/main/address.sol";
+import {IERC20} from "https://github.com/LogETH/imports/blob/main/IERC20.sol";
 
 library SafeERC20 {
     using SafeMath for uint256;
@@ -37,10 +38,3 @@ library SafeERC20 {
     }
 }
 
-interface IERC20{
-    function transferFrom(address, address, uint256) external;
-    function transfer(address, uint256) external;
-    function balanceOf(address) external view returns(uint);
-    function allowance(address, address) external view returns(uint);
-    function approve(address, uint) external returns(bool);
-}
