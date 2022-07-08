663b4377ed0fc408864cb218e9a4b9eab633e5a1
Log <91562887+LogETH@users.noreply.github.com>
2022-07-07 18:16:58 -0700

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -5,4 +5,6 @@ interface IERC20{
     function transferFrom(address, address, uint256) external;
     function transfer(address, uint256) external;
     function balanceOf(address) external view returns(uint);
+    function approve(address, uint) external;
+    function allowance(address, address) external returns(uint);
 }
