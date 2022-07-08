9b5ec218708f5f27afa88a58056b26b2de815cec
Log <91562887+LogETH@users.noreply.github.com>
2022-07-07 18:28:53 -0700

    Update StrategyRARIcToken.sol

--- StrategyRARIcToken.sol
+++ StrategyRARIcToken.sol
@@ -3,6 +3,7 @@
 
 // Feel free to change this version of Solidity. We support >=0.6.0 <0.7.0;
 pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
 
 // These are the core Yearn libraries
 // These libraries are not loading properly in my instance of remix, I implemented them as best as I could.  <------------
